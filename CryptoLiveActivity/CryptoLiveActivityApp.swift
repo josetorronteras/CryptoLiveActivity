72107a08a2f304b1e87a0899f2ecd6baea42fa01
Jose Torronteras <7280807+josetorronteras@users.noreply.github.com>
2024-02-12 23:32:02 +0100

    chore: project creation

---
+++ CryptoLiveActivity/CryptoLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoLiveActivityApp.swift
+//  CryptoLiveActivity
+//
+//  Created by Jose Torronteras on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
